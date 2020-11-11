2bb5d286559646d6b7b4fe451f901e3500b14cb9
phatdroid94 <phatdroid94@gmail.com>
2020-11-11 18:28:52 +0700

    kieu du lieu

--- buoi1.js
+++ buoi1.js
@@ -1,7 +1,14 @@
 // ctrl + ~
+// github : https://github.com/phamtanphat
+// npm install nodemon -g
 // 1 : Khai bao bien
 // mutable vs immutable
-var a = 5
-const b = "10"
-b  = "11"
-console.log(b)
+// var a = 5
+// const b = "10"
+// console.log(b)
+
+// 2 Kiểu dữ liệu
+// Các trường hợp undefined
+// TH1 : Khai báo bằng từ khóa var nhưng không gán giá trị
+// var a 
+// console.log(a)
