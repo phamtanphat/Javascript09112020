54e783ccf9356ad952b135da0b073160e7355200
phatdroid94 <phatdroid94@gmail.com>
2020-11-13 18:51:08 +0700

    bai tap for in

--- buoi2.js
+++ buoi2.js
@@ -20,12 +20,45 @@
 
 // console.log(multiply([2, 3, 4])); // expect: 24
 
-const arrNums = [5,1,2,10,20,15]
-const teo = {
-    name : "Nguyen Van Teo",
-    age : 10
+// const arrNums = [5,1,2,10,20,15]
+// const teo = {
+//     name : "Nguyen Van Teo",
+//     age : 10
+// }
+
+// for (const key in teo) {
+//     console.log(key)
+// }
+
+var apartment = {
+  bedroom: {
+    area: 20,
+    bed: {
+      type: 'twin-bed',
+      price: 100
+    }
+  }
+};
+
+function getkey(apartment){
+    for (const key in apartment) {
+        console.log(key)
+        if (typeof apartment[key] == 'object'){
+            getkey(apartment[key])
+        }
+    }
 }
 
-for (const key in teo) {
-    console.log(key)
-}
+getkey(apartment)
+
+/**
+ * Kết quả mong muốn:
+ * bedroom
+ * area
+ * bed
+ * type
+ * price
+ * Chú ý: không cần hiển thị ra đúng thứ tự như trên
+ */
+
+
