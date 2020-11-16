c03bb720f252fb4731d5437687edb1c0d52336ff
phatdroid94 <phatdroid94@gmail.com>
2020-11-16 18:30:04 +0700

    ham filter

--- buoi3.js
+++ buoi3.js
@@ -1,5 +1,5 @@
 // 15 : Ham map
-const arrNumbers = [2,4,6,8,10,12]
+// const arrNumbers = [2,4,6,8,10,12]
 
 // for (const key in arrNumbers) {
 //     arrNumbers[key] *= 2   
@@ -27,33 +27,43 @@ const arrNumbers = [2,4,6,8,10,12]
 
 // Ví dụ
 // Make an array of strings of the names
-function namesOnly(arr) {
-    const newArrNames =  arr.map(function(person){
-        return person.name
-    })
-    console.log(newArrNames)
-}
+// function namesOnly(arr) {
+//     const newArrNames =  arr.map(function(person){
+//         return person.name
+//     })
+//     console.log(newArrNames)
+// }
 
-namesOnly([
-  {
-    name: "Angelina Jolie",
-    age: 80
-  },
-  {
-    name: "Eric Jones",
-    age: 2
-  },
-  {
-    name: "Paris Hilton",
-    age: 5
-  },
-  {
-    name: "Kayne West",
-    age: 16
-  },
-  {
-    name: "Bob Ziroll",
-    age: 100
-  }
-])
+// namesOnly([
+//   {
+//     name: "Angelina Jolie",
+//     age: 80
+//   },
+//   {
+//     name: "Eric Jones",
+//     age: 2
+//   },
+//   {
+//     name: "Paris Hilton",
+//     age: 5
+//   },
+//   {
+//     name: "Kayne West",
+//     age: 16
+//   },
+//   {
+//     name: "Bob Ziroll",
+//     age: 100
+//   }
+// ])
 // ["Angelina Jolie", "Eric Jones", "Paris Hilton", "Kayne West", "Bob Ziroll"]
+
+
+//16  : Ham filter
+
+const arrNames = ["Teo","Ti","Hoa","Phat","Lan","Khanh"]
+
+const newArrNames = arrNames.filter(function(value){
+    return value.length == 3 ? true : false
+})
+console.log(newArrNames)
