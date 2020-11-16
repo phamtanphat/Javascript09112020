e49bd014f0dfcdb1c93353052ec540e329b24412
phatdroid94 <phatdroid94@gmail.com>
2020-11-16 18:56:50 +0700

    bai tap ham reduce

--- buoi3.js
+++ buoi3.js
@@ -85,10 +85,38 @@
 // console.log(newArrNames)
 
 // 17 : Ham reduce
-const arrNums = [1,2,3,4,5,6,7,8,9,10]
+// const arrNums = [1,2,3,4,5,6,7,8,9,10]
 
-const newArrNums = arrNums.reduce(function(accumulator , currentValue){
-    return accumulator + currentValue
-},0)
+// const newArrNums = arrNums.reduce(function(accumulator , currentValue){
+//     return accumulator + currentValue
+// },0)
 
-console.log(newArrNums)
+// console.log(newArrNums)
+
+
+// Trả về số lượng người đã vote
+function totalVotes(arr) {
+    const countVotes =  arr.reduce(function(accumulator , currentValue){
+        if (currentValue.voted){
+            accumulator++
+        }
+        return accumulator
+    }, 0)
+    console.log(countVotes)
+}
+
+var voters = [
+  {name:'Bob' , age: 30, voted: true},
+  {name:'Jake' , age: 32, voted: true},
+  {name:'Kate' , age: 25, voted: false},
+  {name:'Sam' , age: 20, voted: false},
+  {name:'Phil' , age: 21, voted: true},
+  {name:'Ed' , age:55, voted:true},
+  {name:'Tami' , age: 54, voted:true},
+  {name: 'Mary', age: 31, voted: false},
+  {name: 'Becky', age: 43, voted: false},
+  {name: 'Joey', age: 41, voted: true},
+  {name: 'Jeff', age: 30, voted: true},
+  {name: 'Zack', age: 19, voted: false}
+];
+totalVotes(voters) // 7
