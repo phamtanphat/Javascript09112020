8379ddc1776523437de779d53202383c4a5ce0b5
phatdroid94 <phatdroid94@gmail.com>
2020-11-16 18:17:22 +0700

    cach viet ham map

--- buoi3.js
+++ buoi3.js
@@ -6,8 +6,21 @@ const arrNumbers = [2,4,6,8,10,12]
 // }
 // console.log(arrNumbers)
 
-const newArrNumbers = arrNumbers.map(function(value, index){
+// const newArrNumbers = arrNumbers.map(function(value, index){
+//     return value * 2
+// })
+
+// console.log(newArrNumbers)
+
+Array.prototype.myMap = function(cb){
+    const newArr = []
+    for (var i = 0 ; i < this.length ; i++){
+        newArr.push(cb(this[i] , i ))
+    }
+    return newArr
+}
+
+const newArrNumbers = arrNumbers.myMap(function(value, index){
     return value * 2
 })
-
 console.log(newArrNumbers)
