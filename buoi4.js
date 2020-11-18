f10507db5243284f65361462df19e02718dc2550
phatdroid94 <phatdroid94@gmail.com>
2020-11-18 18:13:55 +0700

    ternary operator

---
+++ buoi4.js
@@ -0,0 +1,14 @@
+// 21 : Ternary operator
+// const a = 7
+// const b = 6
+
+// a > b = 1
+// a < b = -1
+// a = b = 0
+// if (a > b){
+//     console.log("A lon hon B")
+// }else{
+//     console.log("A be hoac bang B")
+// }
+// const result = a > b ? 1 : a < b ? -1 : 0
+// console.log(result)
