67e322da9e718242977e11926531f4d8d8f1254e
phatdroid94 <phatdroid94@gmail.com>
2020-11-18 18:51:53 +0700

    function context va bind

--- buoi4.js
+++ buoi4.js
@@ -53,5 +53,34 @@
 // }
 // console.log(a)
 
+// 24 : Function context and bind
+
+// var mouse = {
+//     name : 'Mickey',
+//     sayhi : function() {
+//       console.log('Hi, my name is' , this.name)
+//     }
+//   }
+  
+// var sayCoppy = mouse.sayhi.bind(mouse)
+// sayCoppy()
+
+function run(cb){
+    console.log('run...')
+    cb()
+  }
+  
+const mouse = {
+    name : 'Mickey',
+    run : function() {
+        console.log(this.name + ' is running')
+    }
+}
+
+run(mouse.run.bind(mouse))
+  
+  
+
+
 
 
