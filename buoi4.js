4627725e11c09266d2a4473b9038db16d064f1d9
phatdroid94 <phatdroid94@gmail.com>
2020-11-18 18:26:21 +0700

    function scope

--- buoi4.js
+++ buoi4.js
@@ -12,3 +12,39 @@
 // }
 // const result = a > b ? 1 : a < b ? -1 : 0
 // console.log(result)
+
+// 22 : Function scope
+
+// var a = 1
+// function random(){
+//   var a = Math.random()
+//   console.log(a)
+// }
+// random()
+// console.log(a)
+
+// var a = 1
+// function random(){
+//   var a = Math.random()
+//   var b = 5
+// }
+// random()
+// console.log(b)
+
+var a = 1;
+
+// var b = {
+//   a: 2,
+//   foo: function() {
+//     console.log(this.a);
+//   }
+// };
+
+// b.foo();
+
+// var fooCopy = b.foo;
+// fooCopy();
+// console.log(this)
+
+
+
